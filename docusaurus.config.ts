ab6f4187f144efb188e4e96337406edb220554fa
Lars Hobusch <4blue@gmx.de>
2026-03-20 21:50:54 +0100

    fix: URL auf docs.philatelie-manager.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://handbuch.philatelie-manager.com',
+  url: 'https://docs.philatelie-manager.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
